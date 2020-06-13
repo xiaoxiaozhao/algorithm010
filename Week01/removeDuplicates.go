6b6a1f3c7164f47e3ce110ee0030c9ea8e5da3c6
xiaoxiaozhao <783044971@qq.com>
2020-06-13 17:29:10 +0800

    changed

--- Week01/removeDuplicates.go
+++ Week01/removeDuplicates.go
@@ -1,6 +1,7 @@
 package Week01
 
-
+//直接比较自己与自己相邻的数据的大小
+//缺点，每次都要重新生成数组 效率较低
 func removeDuplicates(nums []int) int {
 	if len(nums) <=1{
 		return len(nums)
@@ -11,4 +12,21 @@ func removeDuplicates(nums []int) int {
 		}
 	}
 	return len(nums)
+}
+
+//双指针法进行移动
+func removeDuplicates_1(nums []int) int {
+	if len(nums) <=1{
+		return len(nums)
+	}
+	left := 1
+	right :=1
+	for right < len(nums)-1 {
+		if nums[right-1] != nums[right]{
+			nums[left] = nums[right]
+			left++
+		}
+		right++
+	}
+	return left
 }
