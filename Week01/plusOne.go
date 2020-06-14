66ece4d26c51ebec9154f840b9cb036097a988b2
xiaoxiaozhao <783044971@qq.com>
2020-06-14 15:59:09 +0800

    changed

---
+++ Week01/plusOne.go
@@ -0,0 +1,27 @@
+package Week01
+func plusOne(digits []int) []int {
+	if len(digits)==0{
+		return []int{1}
+	}
+	//从最高位开始递增
+	p := len(digits)-1
+	var result []int
+	for {
+		if digits[p]==9{
+			if p == 0{
+				result = append(result,1)
+				digits[p] = 0
+				result = append(result,digits...)
+				break
+			}else {
+				digits[p] = 0
+				p--
+			}
+		}else {
+			digits[p] = digits[p]+1
+			result = append(result,digits...)
+			break
+		}
+	}
+	return result
+}
