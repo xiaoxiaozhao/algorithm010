7d3cfbdf30e06794dfbcf98baa9e5823ac7c34fe
xiaoxiaozhao <783044971@qq.com>
2020-07-05 17:44:51 +0800

    changed

---
+++ Week04/canJump.go
@@ -0,0 +1,25 @@
+package Week04
+func canJump(nums []int) bool {
+	if len(nums) == 0{
+		return true
+	}
+	rightMax := 0
+	for idx,value:=range nums{
+		if idx <= rightMax{
+			rightMax = max(rightMax,value+idx)
+			if rightMax >= len(nums)-1{
+				return true
+			}
+		}
+	}
+	return false
+
+
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+
+}
